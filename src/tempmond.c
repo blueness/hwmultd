cfc10afd5d27641fcc79cf64d7d2d343c0a92714
Anthony G. Basile <basile@opensource.dyc.edu>
2011-04-01 11:52:52 -0400

    src/tempmond.c: Added code for stub daemon.

--- src/tempmond.c
+++ src/tempmond.c
@@ -1,4 +1,41 @@
 
-int main() {
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int
+main( int argc, char *argv[] )
+{
+	/* Fork into background */
+	pid_t pid = fork();
+
+	if (pid < 0)
+	{
+		fprintf( stderr, "Failed to fork\n") ;
+		exit(EXIT_FAILURE);
+	}
+
+	if (pid > 0)
+		exit(EXIT_SUCCESS);
+
+	pid = getpid() ;
+
+	/* Close open files inherited from parent. */
+	close(STDIN_FILENO);
+	close(STDOUT_FILENO);
+	close(STDERR_FILENO);
+
+	fprintf( stdout, "I am stdout\n") ;
+	fprintf( stderr, "I am stderr\n") ;
+
+	/* Create a new SID for the child process */
+	pid_t sid = setsid();
+	if (sid < 0)
+	{
+		exit(EXIT_FAILURE);
+	}
+
+	for(;;);
+
 	return 0;
 }
