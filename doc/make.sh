1f884093d88ecc4d3fe8e3663797b5dc7cdfb271
Anthony G. Basile <blueness@gentoo.org>
2011-07-14 17:17:21 -0400

    doc/hwmultd.8: added documentation

--- doc/make.sh
+++ doc/make.sh
@@ -2,4 +2,8 @@
 
 #Run this on developer side, and distribute troff
 #in case the end user doesn't have pod2man
+rm -f hwmultd.8
 pod2man hwmultd.pod > hwmultd.8
+sed -i	-e 's|perl v5.12.3|HWMULTD(1)|' \
+	-e 's|User Contributed Perl Documentation|SYSTEM ADMINISTRATION COMMANDS|' \
+	hwmultd.8
