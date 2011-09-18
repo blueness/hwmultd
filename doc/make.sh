fb1c5c4826c0b92155a9ee6cb07ea0bc21953fe3
Anthony G. Basile <blueness@gentoo.org>
2011-09-18 11:18:25 -0400

    doc/make.sh: improved pod2man script

--- doc/make.sh
+++ doc/make.sh
@@ -2,8 +2,13 @@
 
 #Run this on developer side, and distribute troff
 #in case the end user doesn't have pod2man
+
 rm -f hwmultd.8
-pod2man hwmultd.pod > hwmultd.8
-sed -i	-e 's|perl v5.12.3|HWMULTD(1)|' \
-	-e 's|User Contributed Perl Documentation|SYSTEM ADMINISTRATION COMMANDS|' \
-	hwmultd.8
+
+pod2man \
+ --official \
+ --section="8" \
+ --release="hwmultd 0.1" \
+ --center="SYSTEM ADMINISTRATION COMMANDS" \
+ --date="2011-07-19" \
+ hwmultd.pod > hwmultd.8
